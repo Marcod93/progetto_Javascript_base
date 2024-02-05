bc2b560d9971fcd372c162f2fd857f4b87e5d24d
Marco <marcodonati93@libero.it>
2024-02-05 19:09:24 +0100

    aggiunto icone come pulsanti, creato footer, minimizzato file js

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,38 +1,27 @@
 function toggleTheme(theme) {
-    const bodyClassList = document.body.classList;
+  document.body.classList.remove('dark', 'light');
+  document.body.classList.add(theme);
+}
 
-    if (theme === 'light' && !bodyClassList.contains('light')) {
-      bodyClassList.remove('dark');
-      bodyClassList.add('light');
-    } else if (theme === 'dark' && !bodyClassList.contains('dark')) {
-      bodyClassList.remove('light');
-      bodyClassList.add('dark');
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => toggleTheme(btn.id));
+});
+
+function counter(btnCounter) {
+  const resultElement = document.getElementById('result');
+  let resultNumber = Number(resultElement.innerText);
+
+  if (btnCounter === 'plus') {
+    resultElement.innerText = String(resultNumber + 1);
+  } else if (btnCounter === 'minus') {
+    if (resultNumber === 0) {
+      alert("Gia stai a 0!");
+      return;
     }
+    resultElement.innerText = String(resultNumber - 1);
   }
+}
 
-  document.querySelectorAll('.btn').forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      toggleTheme(btn.id);
-    });
-  });
-
-  function counter(btnCounter) {
-    let resultElement = document.getElementById('result');
-    let resultNumber = Number(resultElement.innerText);
-  
-    if (btnCounter === 'plus') {
-      resultElement.innerText = String(resultNumber + 1);
-    } else if (btnCounter === 'minus') {
-      if(resultNumber == 0){
-        alert("Gia stai a 0!");
-        return
-      }
-      else
-      resultElement.innerText = String(resultNumber - 1);
-  }}
-  
-  document.querySelectorAll('.btnCounter').forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      counter(btn.id);
-    })
-  });
+document.querySelectorAll('.btnCounter').forEach(btn => {
+  btn.addEventListener('click', () => counter(btn.id));
+});
